d326740bc922ea31fac163679a21e0e30306e64b
Sudheer Vadrevu <vadrevusudheer@gmail.com>
2026-06-16 12:00:58 +0200

    install-docker.sh

---
+++ project/install-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R="\e[31e"
+N="\e[0e"
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl enable nginx
+systemctl start nginx
+usermod -aG docker ec2-user
+echo -e "$R login and logoutagain $N"
